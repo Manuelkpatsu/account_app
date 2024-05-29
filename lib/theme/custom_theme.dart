7eba767274896328c05705cb72ecc94a423a44a9
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2024-05-29 20:13:16 +0000

    Change text style of AppBar

--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -6,17 +6,21 @@ import 'custom_color.dart';
 class CustomTheme {
   static ThemeData get theme {
     return ThemeData(
-      scaffoldBackgroundColor: CustomColor.backgroundColor,
-      primaryColor: CustomColor.blueAccent,
-      canvasColor: Colors.transparent,
-      appBarTheme: const AppBarTheme(
-        elevation: 0,
-        backgroundColor: CustomColor.backgroundColor,
-        foregroundColor: Colors.black,
-        iconTheme: IconThemeData(color: Colors.black),
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-      ),
-      fontFamily: 'SFPro'
-    );
+        scaffoldBackgroundColor: CustomColor.backgroundColor,
+        primaryColor: CustomColor.blueAccent,
+        canvasColor: Colors.transparent,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: CustomColor.backgroundColor,
+          foregroundColor: CustomColor.titleColor,
+          iconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.bold,
+            color: CustomColor.titleColor,
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+        ),
+        fontFamily: 'SFPro');
   }
 }
